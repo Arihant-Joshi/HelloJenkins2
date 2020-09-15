ff4b96dc40e31c78f2e04c4f99c168e81c2575df
Arihant Joshi <arihant.joshi@infoworks.io>
2020-09-15 11:44:57 +0530

    FirstCommit

---
+++ hi.py
@@ -0,0 +1,16 @@
+import sys
+
+def main():
+	print(len(sys.argv))
+	name = "Jenkins User"
+	count = 1
+	if(len(sys.argv) == 3):
+		count = int(sys.argv[2])
+	if(len(sys.argv) >= 2):
+		name = sys.argv[1]
+
+	for i in range(count):
+		print("Hello2~",str(name),i)
+
+if __name__ == "__main__":
+	main()
